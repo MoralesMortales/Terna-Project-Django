dc24337928390ef46944f0f2a1ed604dfabdf021
CarlosMorales <carlos52.7624@gmail.com>
2024-03-26 14:59:39 -0400

    template 0.1 v

--- TernaApp/urls.py
+++
@@ -1,6 +0,0 @@
-from .views import BasePage
-from django.urls import path
-
-urlpatterns = [
-   path("", BasePage.as_view(), name="Base")
-]
--- TernaApp/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.views.generic import TemplateView
-# Create your views here.
-
-
-class BasePage(TemplateView):
-    template_name = "base.html"
-
-
