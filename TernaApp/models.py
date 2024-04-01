b835be59953d546e3a01dda17e50736d9cb4a296
CarlosMorales <carlos52.7624@gmail.com>
2024-04-01 18:46:39 -0400

    users enable

--- TernaApp/models.py
+++ TernaApp/models.py
@@ -1,13 +1,2 @@
 from django.db import models
 from django.contrib.auth.models import User
-
-classes=[('one','one'),('two','two'),('three','three'),
-('four','four'),('five','five'),('six','six'),('seven','seven'),('eight','eight'),('nine','nine'),('ten','ten')]
-
-class StudentExtra(models.Model):
-    user=models.OneToOneField(User,on_delete=models.CASCADE)
-    roll = models.CharField(max_length=10)
-    mobile = models.CharField(max_length=40,null=True)
-    fee=models.PositiveIntegerField(null=True)
-    cl= models.CharField(max_length=10,choices=classes,default='one')
-    status=models.BooleanField(default=False)
--- TernaApp/views.py
+++ TernaApp/views.py
@@ -9,7 +9,12 @@ from . import forms,models
 from django.contrib.auth.models import User
 from django.contrib import messages
 
+from django.contrib.auth import logout, login, authenticate
+
+
 from django.core.mail import send_mail
+
+
 # Create your views here.
 
 
@@ -20,22 +25,46 @@ class FormularioEmail(TemplateView):
     template_name = "formularioContacto.html"
 
 def logIn(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+    
+        myuser = authenticate(username = username, password = password)
+        
+        if myuser is not None:
+            login(request,myuser)
+            return render(request,"menu.html", {'username':username})
+            
+        else:
+            messages.error(request, 'Wrong username or password')
+            return render(request,"login.html")
+            
     return render(request,"login.html")
 
 def logOut(request):
-    return render(request,"logout.html")
+    if request.method=="POST":   
+        logout(request)
+        return render(request,"menu.html")
+        
+    
+    return render(request,"signout.html")
 
 def signUp(request):
     
     if request.method == "POST":
         
         username = request.POST['username']
+        email = request.POST['email']
         password = request.POST['password']
         password_conf = request.POST['password_conf']
         
         if password == password_conf:
               
-            myuser = User.objects.create_user(username, password)
+            myuser = User.objects.create_user(username, email, password)
+            
+            myuser.username = username
+            myuser.email = email
+            
             myuser.save()
             
             messages.success(request, "Congrats, you have signed up")
