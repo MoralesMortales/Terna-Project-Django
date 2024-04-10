a6a9e2a55fd6805b68268bbb37e377a40944bef3
CarlosMorales <carlos52.7624@gmail.com>
2024-04-09 21:42:20 -0400

    0.6 bp

--- TernaProject/settings.py
+++ TernaProject/settings.py
@@ -79,9 +79,13 @@ WSGI_APPLICATION = 'TernaProject.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': "terna.db",
+       'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'terna_db',
+        'USER': 'root',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',  # or the hostname where your MySQL server is running
+        'PORT': '3306',      # or the port on which your MySQL server is listening
     }
 }
 
